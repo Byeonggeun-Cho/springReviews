ee09fc43b89c495aa121b034d65fd2519b335580
qptjtt <qptjtt@gmail.com>
2021-01-10 16:41:40 +0900

    ## spring04mybatis
    . mapper 등록
    . 파라미터 전달
    	1. map
    	2. vo객체

--- spring04mybatis/src/test/java/com/kh/spring04/Test02.java
+++ spring04mybatis/src/test/java/com/kh/spring04/Test02.java
@@ -2,8 +2,6 @@ package com.kh.spring04;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -11,8 +9,10 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
+import com.kh.spring04.entity.Student;
+
 // mybatis에 필요한 도구들을 연동 없이 직접 생성 - 독립 테스트
-public class Test01 {
+public class Test02 {
 	
 	@Test
 	public void test() throws IOException {
@@ -29,14 +29,12 @@ public class Test01 {
 		//	(1) Map으로 묶어서 전달
 		//	(2) 클래스의 객체(vo)로 묶어서 전달
 		
-		String name = "피카츄";
-		int score = 70;
+		Student student = new Student().builder()
+									.name("파이리")
+									.score(85)
+									.build();
 		
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("name", name);
-		map.put("score", score);
-		
-		sqlSession.insert("student.add", map);
+		sqlSession.insert("student.add2", student);
 		
 	}
 	
