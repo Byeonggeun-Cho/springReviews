5a8c96f4b492c41a2831da834ae532ee32568516
qptjtt <qptjtt@gmail.com>
2021-01-11 17:02:16 +0900

    ## spring06connect
    . 오류 수정
    	- mapper에서 존재하지 않는 타입 참조하여 발생한 오류 수정

---
+++ spring06connect/src/test/java/com/kh/spring06/Test01.java
@@ -0,0 +1,27 @@
+package com.kh.spring06;
+
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+// 연동 설정 후 sqlSession을 사용할 수 있는지 확인
+
+@RunWith(SpringJUnit4ClassRunner.class)	// 스프링과 연동할 것을 선언
+// 스프링 설정 파일 연결
+// 프로젝트 root : file
+@ContextConfiguration(locations = {
+		"file:src/main/webapp/WEB-INF/spring/root-context.xml"
+})
+public class Test01 {
+
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@Test
+	public void test() {
+		System.out.println(sqlSession);
+	}
+}
