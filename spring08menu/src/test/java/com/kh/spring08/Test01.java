90c7c8d5c6156b66b7fdfa61f67e89f40101479a
qptjtt <qptjtt@gmail.com>
2021-01-14 23:39:37 +0900

    ## spring08menu
    . log4j 설정

--- spring08menu/src/test/java/com/kh/spring08/Test01.java
+++ spring08menu/src/test/java/com/kh/spring08/Test01.java
@@ -1,5 +1,28 @@
 package com.kh.spring08;
 
-public class Test01 {
+import java.time.LocalDateTime;
 
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test01 {
+	
+	// Loger(로깅 도구 생성)
+	// - import는 무조건 org.slf4j를 선택
+	Logger log = LoggerFactory.getLogger(Test01.class);
+	
+	@Test
+	public void test() {
+		// 표준 출력
+		// - 어느 위치에서 출력했는지 알 수 없음
+		// - 언제 출력했는지 알 수 없음
+		// - 언젠가는 지워야 함
+		// - 출력 위치를 정할 수 없음(무조건 콘솔에 출력됨)
+		
+		System.out.println(LocalDateTime.now() + "Hello world!");
+		
+		// Logger를 이용한 출력
+		log.info("Hello world!");
+	}
 }
