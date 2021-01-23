ddcbc0b184099282fe6d4831baae85ce8cc56a59
qptjtt <qptjtt@gmail.com>
2021-01-23 22:28:47 +0900

    ## spring14middleware
    . 뷰 페이지 & 컨트롤러 준비

--- spring14middleware/src/main/java/com/kh/spring14/HomeController.java
+++ spring14middleware/src/main/java/com/kh/spring14/HomeController.java
@@ -4,10 +4,13 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -36,4 +39,42 @@ public class HomeController {
 		return "home";
 	}
 	
+	// 로그인 처리 컨트롤러
+	// - 세션에 user라는 이름으로 admin이라는 값을 저장(샘플 아이디)
+	// - 루트 페이지(/)로 리다이렉트
+	
+	@GetMapping("/login")
+	public String login(HttpSession session) {
+		session.setAttribute("user", "admin");
+		
+		return "redirect:/";
+	}
+	
+	// 로그아웃 처리 컨트롤러
+	// - 세션에 있는 user 정보를 삭제
+	// - 루트 페이지(/)로 리다이렉트
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		// session.invalidate();			// session이 삭제 됨
+		session.removeAttribute("user");	// session 내 특정 속성값만 삭제
+		
+		return "redirect:/";
+	}
+	
+	// 회원 전용 페이지
+	@GetMapping("/member")
+	public String member() {
+		return "member";		// WEB-INF/views/member.jsp
+	}
+	
+	// 일반 페이지
+	@GetMapping("/normal")
+	public String normal() {
+		return "normal";		// WEB-INF/views/normal.jsp
+	}
 }
+
+
+
+
+
--- spring14middleware/src/main/webapp/WEB-INF/views/home.jsp
+++ spring14middleware/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,14 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
-<html>
-<head>
-	<title>Home</title>
-</head>
-<body>
-<h1>
-	Hello world!  
-</h1>
+<%@ page contentType="text/html; charset=UTF-8" %>
 
-<P>  The time on the server is ${serverTime}. </P>
+<body>
+	<h1>테스트 페이지</h1>
+	
+	<h2>로그인 여부: ${user != null}, ${!empty user}, ${not empty user} </h2>
+	<h2>로그인 정보: ${sessionScope.user}, ${user}</h2>
+	
+	<h2><a href="login">로그인</a></h2>
+	<h2><a href="logout">로그아웃</a></h2>
+	
+	<h2><a href="member">회원 전용 페이지</a></h2>
+	<h2><a href="normal">일반 페이지</a></h2>
 </body>
-</html>
