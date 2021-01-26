aeb95ddd163cf136252b0392362d0c29aa76542d
qptjtt <qptjtt@gmail.com>
2021-01-26 20:07:14 +0900

    ## spring15email
    . spring 연동 테스트 환경 구축

---
+++ spring15email/src/test/java/com/kh/spring15/Test02.java
@@ -0,0 +1,21 @@
+package com.kh.spring15;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations= {
+		"file:src/main/webapp/WEB-INF/spring/root-context.xml",
+		"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"
+})
+@WebAppConfiguration
+public class Test02 {
+
+	@Autowired
+	private JavaMailSender sender;
+	
+}
