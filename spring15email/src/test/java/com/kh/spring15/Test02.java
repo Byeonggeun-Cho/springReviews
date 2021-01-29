8a56fbe5f0c38d7885f1587bbf982729f8daf805
qptjtt <qptjtt@gmail.com>
2021-01-30 00:11:19 +0900

    ## spring15email
    . 정보 별도 보관
    	- *.properties로 별도 보관
    . 추가 메세지 기능
    	- 모양, 이미지, 첨부파일
    . 인증시스템

--- spring15email/src/test/java/com/kh/spring15/Test02.java
+++ spring15email/src/test/java/com/kh/spring15/Test02.java
@@ -1,7 +1,9 @@
 package com.kh.spring15;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -17,5 +19,35 @@ public class Test02 {
 
 	@Autowired
 	private JavaMailSender sender;
-	
+
+	@Test
+	public void test() {
+		// 메일 전송
+		// 1. 간단한 단문 메세지:  SimpleMailMessage
+		// SimpleMailMessage 객체에 값을 넣어 전송
+		
+		SimpleMailMessage message = new SimpleMailMessage();
+		
+		// 제목
+		message.setSubject("이번주 로또 번호는!?");
+
+		// 수신인
+		String[] to = {"pumpkin.cstem@gmail.com"};
+		message.setTo(to);
+		
+		// 참조(cc, carbon copy)
+		String[] cc = {};
+		message.setCc(cc);
+		
+		// 숨은참조(bcc, blind carbon copy)
+		String[] bcc = {"qptjtt@naver.com"};
+		message.setBcc(bcc);
+		
+		// 내용
+		String text = "나도 모름ㅋ";
+		message.setText(text);
+		
+		// 전송
+		sender.send(message);
+	}
 }
