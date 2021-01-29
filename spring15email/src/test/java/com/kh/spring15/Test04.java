ff21fb2e24b46d11bb31a5355a29b040fe15320a
qptjtt <qptjtt@gmail.com>
2021-01-30 00:51:15 +0900

    ## spring15email
    . 정보 별도 보관
    	- *.properties로 별도 보관
    . 추가 메세지 기능
    	- 모양, 이미지, 첨부파일
    		- MimeMessage 타입 사용
    		- MimeMessageHelper 이용하여 설정
    		- HTML 템플릿 전송
    			- 별도의 템플릿 파일 생성
    			- ClassPathResource로 불러옴
    			- BufferedReader를 이용해 읽어들임
    			- String 타입으로 메세지에 전달 / HTML 설정(true)
    . 인증시스템

--- spring15email/src/test/java/com/kh/spring15/Test04.java
+++ spring15email/src/test/java/com/kh/spring15/Test04.java
@@ -1,11 +1,17 @@
 package com.kh.spring15;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.test.context.ContextConfiguration;
@@ -18,13 +24,15 @@ import org.springframework.test.context.web.WebAppConfiguration;
 		"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"
 })
 @WebAppConfiguration
-public class Test03 {
+public class Test04 {
 	@Autowired
 	private JavaMailSender sender;
 	
-	// 목표: 복잡한 메세지(MimeMessage)를 만들어 전송
+	// 목표: 미리 만들어둔 템플릿(template.html)을 불러와서 전송
+	// - 비밀번호 변경 링크
+	// - 회원가입 환영 메세지
 	@Test
-	public void test() throws MessagingException {
+	public void test() throws MessagingException, IOException {
 		// 글자만 전송 가능
 		// SimpleMailMessage message = new SimpleMailMessage();
 
@@ -47,7 +55,30 @@ public class Test03 {
 		String[] bcc = {};
 		helper.setBcc(bcc);
 		
-		helper.setText("<h1>이 편지는 영국...</h1>", true);		// html 모드 설정
+		// 파일을 읽어올 도구를 만들어 템플릿 파일을 불러온다.
+		ClassPathResource resource = new ClassPathResource("email/template.html");
+		
+		// 불러온 resource를 java.file 형식으로 변환
+		File file = resource.getFile();
+		
+		// BufferedReader를 이용해 파일을 읽어옴
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		
+		// BufferedReader를 이용해 읽어온 값을 String 형식으로 저장
+		StringBuffer  buffer = new StringBuffer();
+		
+		// reader에서 읽은 한 줄을 buffer에 계속 더한다(문자열 합성기)
+		while(true) {
+			String line = br.readLine();	// 1줄씩 읽고
+			if(line == null) {
+				break;
+			}
+			buffer.append(line);
+		}
+		
+		br.close();
+		
+		helper.setText(buffer.toString(), true);		// html 모드 설정
 		
 		// 전송
 		sender.send(message);
