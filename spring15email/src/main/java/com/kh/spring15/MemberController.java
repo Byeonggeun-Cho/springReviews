749da66917fd65aa1367eaadea160015d0c1f015
qptjtt <qptjtt@gmail.com>
2021-02-02 13:16:16 +0900

    ## spring15email
    . 인증번호 발송요청 컨트롤러 생성

---
+++ spring15email/src/main/java/com/kh/spring15/MemberController.java
@@ -0,0 +1,42 @@
+package com.kh.spring15;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.kh.spring15.entity.Member;
+import com.kh.spring15.repository.MemberDao;
+
+@Controller
+public class MemberController {
+	@Autowired
+	private MemberDao memberDao;
+	
+	// 로그인 요청: 임시 아이디로 로그인
+	@GetMapping("/login")
+	public String login(HttpSession session) {
+		Member member = Member.builder()
+								.id("qptjtt@naver.com")
+								.pw("b1234")
+								.build();
+		
+		Member find = memberDao.login(member);
+		
+		if(find != null) {
+			session.setAttribute("user", find);
+		}
+		
+		return "redirect:/";
+	};
+	
+	// 로그아웃 요청
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("user");
+		
+		return "redirect:/";
+	}
+	
+}
--- spring15email/src/main/webapp/WEB-INF/views/home.jsp
+++ spring15email/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,18 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
-<html>
-<head>
-	<title>Home</title>
-</head>
-<body>
-<h1>
-	Hello world!  
-</h1>
+<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 
-<P>  The time on the server is ${serverTime}. </P>
-</body>
-</html>
+<h1>로그인 정보: ${user}</h1>
+
+<h1><a href="login">로그인</a></h1>
+<h1><a href="logout">로그아웃</a></h1>
+
+<hr>
+
+<!-- 로그인 상태일 때 인증번호 보내기 버튼과 인증번호 입력창을 표시 -->
+<c:if test="${not empty user }">
+	<!-- 인증번호 컨트롤러 -> RestController로 처리 -->
+	<button>인증번호 보내기</button>
+	<br>
+	<input type="text"/>
+	<button>확인</button>
+</c:if>
