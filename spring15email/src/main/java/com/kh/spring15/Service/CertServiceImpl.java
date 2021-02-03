2eb719183b953327adae6b95934fb7fb9d848b07
qptjtt <qptjtt@gmail.com>
2021-02-03 15:56:05 +0900

    ## spring15email
    . scheduled 구문 실행시간 수정

--- spring15email/src/main/java/com/kh/spring15/Service/CertServiceImpl.java
+++ spring15email/src/main/java/com/kh/spring15/Service/CertServiceImpl.java
@@ -1,7 +1,6 @@
 package com.kh.spring15.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import com.kh.spring15.entity.Cert;
