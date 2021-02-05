69533287c90e8bb2ac635605c2183ddae689b144
qptjtt <qptjtt@gmail.com>
2021-02-05 22:52:38 +0900

    ## spring16websocket
    . 진행순서
    	1. 프론트엔드 - 서버 연결여부 확인
    		- 콜백(callback) 설정으로 코드 작성
    		- onopen/close/erroe/message
    	2. 메세지 교환 방식 확인
    	3. 사용자가 다수인 경우 관리방식
    	4. 사용자가 다수인 경우 메세지 교환 방식
    	5. text 메세지 이외의 요소 교환
    
    . 기초 웹소켓 서버 구현

--- spring16websocket/src/main/java/com/kh/spring16/websocket/WebsocketBasicServer.java
+++ spring16websocket/src/main/java/com/kh/spring16/websocket/WebsocketBasicServer.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
  * Spring에서 Websocket 관련 처리를 수행하는 서버
  * - Controller와 별개로 작동
  * - 사용자 요청처리이므로 servlet-context.xml에 등록
+ * - WebSocketSession을 사용( != HttpSession)
  * 
  * 구현방법
  * 1. 상속
@@ -18,8 +19,19 @@ import lombok.extern.slf4j.Slf4j;
  * 
  * 2. 메소드 재정의
  * 	- afterConnectionEstablished: 연결이 수립된 직후 실행되는 메소드
- * 	- handleTextMessage: 
- * 	- afterConnectionClosed: 연결이 종료된 직후 실행되는 메소드
+ * 		- WebSocketSession: 웹소켓 접속자 정보
+ * 	
+ *  - handleTextMessage: 
+ * 		- WebSocketSession: 웹소켓 접속자 정보
+ * 		- TextMessage: 수신 메세지 정보
+ * 			- payload: 실제 전송된 텍스트
+ * 			- byteCount: 실제 전송된 데이터 크기
+ * 			- last: 마지막 데이터임을 표시(분할 전송 시). like EOF 
+ * 	
+ *  - afterConnectionClosed: 연결이 종료된 직후 실행되는 메소드
+ * 		- WebSocketSession: 웹소켓 접속자 정보
+ * 		- CloseStatus: 사용자 종료 상태 정보
+ * 
  * 3. 서버를 servlet-context.xml에 등록
  * 
  */
@@ -32,18 +44,31 @@ public class WebsocketBasicServer extends TextWebSocketHandler {
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		log.info("afterConnectionEstablished 실행!");
+		
+		// WebSocketSession: 웹소켓 접속자 정보
+		log.info("session = {}", session);
 		super.afterConnectionEstablished(session);
 	}
 
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 		log.info("handleTextMessage 실행!");
+		
+		log.info("session = {}", session);
+		log.info("message = {}", message);
+		
+		// 메세지를 보낸 사용자에게만 회신(에코, echo)
+		session.sendMessage(message);
+		
 		super.handleTextMessage(session, message);
 	}
 
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
 		log.info("afterConnectionClosed 실행!");
+
+		log.info("session = {}", session);
+		log.info("status = {}", status);
 		super.afterConnectionClosed(session, status);
 	}
 
--- spring16websocket/src/main/webapp/WEB-INF/views/websocket/example1.jsp
+++ spring16websocket/src/main/webapp/WEB-INF/views/websocket/example1.jsp
@@ -5,8 +5,14 @@
 <h1>기본 웹소켓 예제</h1>
 
 <button id="connect">접속버튼</button>
-<hr>
 <button id="disconnect">접속종료</button>
+<hr>
+
+<input type="text" id="user-input" placeholder="메세지 입력">
+<button id="send">전송</button>
+<hr>
+<div class="text-wrapper"></div>
+<hr>
 
 
 <script>
@@ -20,6 +26,59 @@
 			
 			// 웹소켓 연결(실제로는 "소켓을 생성했다"라고 표현)
 			window.socket = new WebSocket(uri);
+		
+			// 연결 이후에 수행할 작업들을 예약(콜백, callback) 설정
+			console.log(socket);		
+			
+			// 연결 후 수행
+			socket.onopen = function(){
+				console.log("연결 완료");
+
+				$("<p>").text("서버에 접속했습니다.").appendTo(".text-wrapper");			
+			};
+
+			// 연결 후 수행
+			socket.onclose = function(){
+				console.log("연결 종료");
+
+				$("<p>").text("서버와의 연결이 종료되었습니다.").appendTo(".text-wrapper");			
+			};
+			
+			// 연결 오류 발생 시 수행
+			socket.onerror = function(){
+				console.log("에러 발생");
+			};
+			
+			
+			// 메세지 도착 시 수행
+			socket.onmessage = function(message){
+				console.log("메세지 수신");
+				
+			//	arguments: 함수(메소드)에서 사용 가능한 매개변수 정보 객체
+			//	console.log(arguments[0]);
+			//	console.log(message.data);
+
+				$("<p>").text("-> " + message.data).appendTo(".text-wrapper");
+			};
+			
+			
+			// 전송 이벤트
+			$("#send").click(function(){
+				// 입력값을 불러온다
+				var input = $("#user-input").val();
+				
+				// 입력값이 없는 경우 function 종료
+				if(!input) return;
+				
+				// 입력값이 있는 경우 나머지 작업 수행
+				// 전송
+				socket.send(input);
+				// Status Code 101로 Message 전송 됨
+				// 개발자 도구에서 메세지 내용 확인 가능
+				
+				// 입력창 초기화
+				$("#user-input").val("");
+			});
 		});
 
 		// 종료 이벤트
