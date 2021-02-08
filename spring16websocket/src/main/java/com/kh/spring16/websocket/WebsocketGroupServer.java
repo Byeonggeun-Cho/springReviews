04ff53aa93d0312c291a8b570ebe8885dbf2d0f9
qptjtt <qptjtt@gmail.com>
2021-02-08 23:59:36 +0900

    ## spring16websocket
    . 그룹서버 구현
    
    . 다수의 접속자 관리 형태
    	1. Set: 전체를 빠르게 관리 -제어
    		- 중복 허용X / 일괄처리(속도 빠름) / 비선형(트리)구조 / 해시구조
    	2. Map: 개별값 관리 특화
    		- key가 set 형태인 저장소 / 비선형 구조
    	3. 순서를 중요시하는 경우
    		- 일렬로 배치해둔 저장소(선형구조)

--- spring16websocket/src/main/java/com/kh/spring16/websocket/WebsocketGroupServer.java
+++ spring16websocket/src/main/java/com/kh/spring16/websocket/WebsocketGroupServer.java
@@ -1,5 +1,8 @@
 package com.kh.spring16.websocket;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
@@ -10,24 +13,35 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class WebsocketGroupServer extends TextWebSocketHandler {
 	
+	// 목표: 접속자를 모아두고 관리하고 싶다.
+	//	- 사용자 인원수는 유동적
+	//	- set<WebSocketSession> 형태로 관리한다
+	private Set<WebSocketSession> users = new HashSet<>();
+	
 	// 접속
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-		// TODO Auto-generated method stub
+		users.add(session);
+		
 		super.afterConnectionEstablished(session);
 	}
 
 	// 종료
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
-		// TODO Auto-generated method stub
+		users.remove(session);
+		
 		super.afterConnectionClosed(session, status);
 	}
 
 	// 수신
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
-		// TODO Auto-generated method stub
+		
+		for(WebSocketSession user : users) {
+			user.sendMessage(message);
+		}
+		
 		super.handleTextMessage(session, message);
 	}
 }
